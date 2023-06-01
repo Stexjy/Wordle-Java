cbbc8d8b9b79c84bee6e80336115735c3a4a2d49
Giuggiola1006 <135206907+Giuggiola1006@users.noreply.github.com>
2023-06-01 09:11:27 +0200

    Uploaded GUI code

---
+++ src/main/java/StartGUI.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class StartGUI extends JFrame {
+    private JLabel title;
+    GameGUI gameGui;
+    private JPanel mainPanel;
+    private GameButton startGame;
+    private JButton log;
+    private JButton leaderboard;
+    private JPanel buttonsPanel;
+
+    public StartGUI() {
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1600,900);
+        add(mainPanel);
+        buttonsPanel.setLayout(new GridLayout(3,1,20,20));
+        setPreferredSize(new Dimension(50,100));
+        setLocationRelativeTo(null);
+    }
+
+    static StartGUI startGui;
+    public static void main(String[] args) {
+        startGui = new StartGUI();
+    }
+}
