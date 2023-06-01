fec9b8e381efeea2f120d8ab3eb0d6dbb88c50a8
Giuggiola1006 <135206907+Giuggiola1006@users.noreply.github.com>
2023-06-01 10:03:32 +0200

    Updated files

---
+++ src/main/java/GameButton.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameButton extends JButton {
+    public GameButton() {
+        setForeground(Color.WHITE);
+        addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startGame();
+            }
+        });
+    }
+
+    public void startGame() {
+        StartGUI.startGui.setVisible(false);
+        StartGUI.startGui.gameGui = new GameGUI();
+    }
+
+}
